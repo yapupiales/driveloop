7250e2a4c6ba09640834adfd96362e6bf23906c2
Jeffer <yapupiales42@soy.sena.edu.com>
2025-11-14 12:21:29 -0500

    Commit 4 Brach Dev Two

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <li class="p-4"><a href="/busqueda-reservas">Test 1</a></li>
         <li class="p-4"><a href="/calificaciones-resenas">Test 2</a></li>
         <li class="p-4"><a href="/contratos-garantias">Test 3 Owner</a></li>
-        <li class="p-4"><a href="/gestion-usuarios">Gestión de Usuarios</a></li>
+        <li class="p-4"><a href="/gestion-usuarios">Test 4 brach dev two</a></li>
         <li class="p-4"><a href="/pagos-digitales">Pagos Digitales</a></li>
         <li class="p-4"><a href="/publicacion-vehiculos">Publicación Vehículos</a></li>
         <li class="p-4"><a href="/soporte-comunicacion">Soporte y Comunicación</li>
